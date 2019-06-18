8e91a5bf04f9a155de30b333ae1c1991e8c4be5c
Roman Kubiak <kubiak.roman@gmail.com>
2019-06-18 20:56:33 +0200

    missing files

---
+++ Source/UI/TwonkFilterPropertiesEditor.cpp
@@ -0,0 +1,16 @@
+/*
+  ==============================================================================
+
+    TwonkFilterPropertiesEditor.cpp
+    Created: 18 Jun 2019 5:38:26pm
+    Author:  rkubiak
+
+  ==============================================================================
+*/
+
+#include "TwonkFilterPropertiesEditor.h"
+
+TwonkFilterPropertiesEditor::TwonkFilterPropertiesEditor(AudioProcessor *owner)
+	: GenericAudioProcessorEditor(owner)
+{
+}
---
+++ Source/UI/TwonkFilterPropertiesEditor.h
@@ -0,0 +1,17 @@
+/*
+  ==============================================================================
+
+    TwonkFilterPropertiesEditor.h
+    Created: 18 Jun 2019 5:38:26pm
+    Author:  rkubiak
+
+  ==============================================================================
+*/
+
+#pragma once
+#include "../TwonkLookAndFeel.h"
+class TwonkFilterPropertiesEditor : public GenericAudioProcessorEditor
+{
+	public:
+		TwonkFilterPropertiesEditor(AudioProcessor *owner);
+};
