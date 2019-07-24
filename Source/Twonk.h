28fcde3525c9a0c173eec29c5d3736c44577d984
RomanKubiak <kubiak.roman@gmail.com>
2019-07-24 22:27:22 +0200

    missing twonk.h

---
+++ Source/Twonk.h
@@ -0,0 +1,19 @@
+/*
+  ==============================================================================
+
+    Twonk.h
+    Created: 17 Jul 2019 2:19:57pm
+    Author:  rkubiak
+
+  ==============================================================================
+*/
+
+#pragma once
+#define TWONK_DEFAULT_TYPEFACE "Liberation Sans"
+#define DEFAULT_PPQN 960.0
+#define TITLEBAR_HEIGHT 48
+#define BUBBLE_SIZE 64
+#define NODE_SIZE 16
+#define NODE_COLOUR_IN  0xff00ff00
+#define NODE_COLOUR_OUT 0xff0000ff
+#define MIDI_KEYBOARD_HEIGHT 0
